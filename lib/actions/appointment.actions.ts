db8c780070d88fe488f50f2a63692190dced5d28
MOSTAFA837 <mostafamanchester837@gmail.com>
2024-07-11 01:46:26 +0200

    appointment success page

--- lib/actions/appointment.actions.ts
+++ lib/actions/appointment.actions.ts
@@ -1,6 +1,6 @@
 "use server";
 
-import { ID } from "node-appwrite";
+import { ID, Query } from "node-appwrite";
 import {
   APPOINTMENT_COLLECTION_ID,
   DATABASE_ID,
@@ -20,6 +20,20 @@ export const CreateAppointment = async (
     );
 
     return parseStringify(newAppointment);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const getAppointment = async (appointmentId: string) => {
+  try {
+    const appointment = await databases.getDocument(
+      DATABASE_ID!,
+      APPOINTMENT_COLLECTION_ID!,
+      appointmentId
+    );
+
+    return parseStringify(appointment);
   } catch (error) {
     console.log(error);
   }
